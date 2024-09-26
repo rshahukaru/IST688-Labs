25af024e20f83d92afa4ddd4b06e632af16f16ad
Revanth Shahukaru <rshahuka@syr.edu>
2024-09-26 15:06:36 +0000

    Started lab_05.py and added requests to requirements.txt, did some minor changes in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ import streamlit as st
 lab_01_page = st.Page("lab_01.py", title="Lab-01")
 lab_02_page = st.Page("lab_02.py", title="Lab-02")
 lab_03_page = st.Page("lab_03.py", title="Lab-03")
-lab_04_page = st.Page("lab_04.py", title="Lab-04", default = True)
+lab_04_page = st.Page("lab_04.py", title="Lab-04")
 
 # The below labs' .py files have not been created yet. So, I am redirecting them to lab-04.py
-lab_05_page = st.Page("lab_05.py", title="Lab-05")
+lab_05_page = st.Page("lab_05.py", title="Lab-05", default = True)
 lab_06_page = st.Page("lab_06.py", title="Lab-06")
 
 # Navigation setup with all homework pages
