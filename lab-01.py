d5398515bf8d4a0564e2873837ca6ab73055054f
Revanth Shahukaru <rshahuka@syr.edu>
2024-09-25 23:10:35 -0400

    Create lab-01.py

---
+++ lab-01.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from openai import OpenAI
+
+# Show title and description.
+st.write("This line is added to :blue[check] whether the changes are being saved or not")
+st.markdown('<h1 style="color: #03B8AC;">LAB-01: Revanth Shahukaru</h1>', unsafe_allow_html=True)
+
+# Retrieve the OpenAI API key from the secrets file
+openai_api_key = st.secrets["openai_api_key"]
+
+# Create an OpenAI client.
+client = OpenAI(api_key=openai_api_key)
+
+# Let the user upload a file via `st.file_uploader`.
+uploaded_file = st.file_uploader(
+    "Upload a document (.txt or .md)", type=("txt", "md")
+)
+
+# Ask the user for a question via `st.text_area`.
+question = st.text_area(
+    "Now ask a question about the document!",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+)
+
+if uploaded_file and question:
+
+    # Process the uploaded file and question.
+    document = uploaded_file.read().decode()
+    messages = [
+        {
+            "role": "user",
+            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+        }
+    ]
+
+    # Generate an answer using the OpenAI API.
+    stream = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        stream=True,
+    )
+
+    # Stream the response to the app using `st.write_stream`.
+    st.write_stream(stream)
+
