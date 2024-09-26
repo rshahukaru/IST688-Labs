663d4181e25b6675a977fafe39332f8be0898afe
Revanth Shahukaru <rshahuka@syr.edu>
2024-09-26 03:28:36 +0000

    created lab 5 and lab 6 files and made some minor changes on streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,17 +9,11 @@ lab_04_page = st.Page("lab-04.py", title="Lab-04", default = True)
 # The below labs' .py files have not been created yet. So, I am redirecting them to lab-04.py
 lab_05_page = st.Page("lab-05.py", title="Lab-05")
 lab_06_page = st.Page("lab-06.py", title="Lab-06")
-lab_07_page = st.Page("lab-07.py", title="Lab-07")
-lab_08_page = st.Page("lab-08.py", title="Lab-08")
-lab_09_page = st.Page("lab-09.py", title="Lab-09")
-lab_10_page = st.Page("lab-10.py", title="Lab-10")
-lab_11_page = st.Page("lab-11.py", title="Lab-11")
 
 # Navigation setup with all homework pages
 pg = st.navigation([
     lab_01_page, lab_02_page, lab_03_page, lab_04_page, lab_05_page,
-    lab_06_page, lab_07_page, lab_08_page, lab_09_page, lab_10_page,
-    lab_11_page
+    lab_06_page
 ])
 
 # Configuration of the main app
